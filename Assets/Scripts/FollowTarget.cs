6e2ae355dfde3bb0f6e8507b2dba53f2ef39189a
Alex Stewart <github@alexstewart.dev>
2023-07-05 19:53:23 -0700

    Smoothed out camera follow to player

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -5,40 +5,21 @@ using UnityEngine;
 public class FollowTarget : MonoBehaviour
 {
     [SerializeField] GameObject Target; 
-    [SerializeField] float SampleRate; 
-    Queue<Vector3> Path; 
-    Vector3 StartPoint; 
+    [SerializeField] float SmoothingSpeed; 
     Vector3 EndPoint;
-    double StartTime;
-    float deltaTime; 
-
-    public AnimationCurve curve; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        Path = new Queue<Vector3>();
-        StartTime = Time.timeAsDouble;
-        StartPoint = transform.position;
         EndPoint = transform.position;
-        deltaTime = 0;
     }
 
     // 
     void Update()
     {
-        deltaTime += Time.deltaTime;
-        if(Time.timeAsDouble - StartTime > SampleRate)
-        {
-            Vector3 temp = Target.transform.position; 
-            temp.z = transform.position.z;
-            Path.Enqueue(temp);
-            
-            StartPoint = EndPoint; 
-            EndPoint = Path.Dequeue();
-            StartTime = Time.timeAsDouble;
-            deltaTime = 0; 
-        }
-        
-        transform.position = Vector3.Lerp(StartPoint, EndPoint, deltaTime/SampleRate);
+        EndPoint = Target.transform.position;
+        EndPoint.z = transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, EndPoint, 
+            SmoothingSpeed * Time.deltaTime);
     }
 }
